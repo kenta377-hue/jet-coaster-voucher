e92b0eaf1247f73eba5758cb1fe41df5a1ee6b60
kenta377-hue <kenta.377@icloud.com>
2025-11-28 23:31:35 +0000

    o

--- functions/src/utils/generateTestToken.ts
+++
@@ -1,9 +0,0 @@
-// development helper - not used in production
-import { signToken } from './utils/tokens';
-
-function gen() {
-  const fakeId = 'TEST-' + Date.now();
-  const token = signToken({ ticketId: fakeId, slotId: 'DEV_0900', ticketNumber: 1, exp: Math.floor(Date.now() / 1000 + 3600) });
-  console.log('TEST TOKEN:', token);
-}
-gen();
